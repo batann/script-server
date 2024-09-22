19a94348f83e3b2e54f07822ec6cbf514b482725
batann <122584289+batann@users.noreply.github.com>
2024-09-22 15:29:47 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-tar vfxz /home/batan/script-server/script-server.tar.gz 
-mv script-server 5
-https://www.swr.de/~podcast/weltspiegel-thema/weltspiegel-podcast-102.xml weld-spiegel 1
-python3 5/script-server/tools/init.py --no-npm
+tar vfxz script-server/script-server.tar.gz
+#mv /home/batan/script-server/script-server.tar.gz /home/batan/
+python3 /home/batan/script-server/tools/init.py --no-npm
 #python3 5/script-server/launcher.py
+clear
+echo -e "\033[31mScript-server installed successfully\033[0m"
+sleep 2
+
